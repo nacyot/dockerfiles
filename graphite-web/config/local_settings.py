27983c094cf2b3320ae06e0a090e0b1806047496
Kim Daekwon <propellerheaven@gmail.com>
2014-07-09 01:00:04 +0900

    Add graphite-web Dockerfile

---
+++ graphite-web/config/local_settings.py
@@ -0,0 +1,12 @@
+import os
+
+LOG_DIR = '/var/log/graphite'
+if os.getenv("CARBONLINK_HOSTS"):
+    CARBONLINK_HOSTS = os.getenv("CARBONLINK_HOSTS").split(',')
+
+if os.getenv("CLUSTER_SERVERS"):
+    CLUSTER_SERVERS = os.getenv("CLUSTER_SERVERS").split(',')
+
+if os.getenv("MEMCACHE_HOSTS"):
+    CLUSTER_SERVERS = os.getenv("MEMCACHE_HOSTS").split(',')
+
