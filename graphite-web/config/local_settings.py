b4d14aaebc1f04ab4df6a4bd59bdf84203e3d84e
Kim Daekwon <propellerheaven@gmail.com>
2014-07-24 22:04:02 +0900

    Update Dockerfiles

--- graphite-web/config/local_settings.py
+++ graphite-web/config/local_settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 LOG_DIR = '/var/log/graphite'
 if os.getenv("CARBONLINK_HOSTS"):
@@ -10,3 +11,4 @@ if os.getenv("CLUSTER_SERVERS"):
 if os.getenv("MEMCACHE_HOSTS"):
     CLUSTER_SERVERS = os.getenv("MEMCACHE_HOSTS").split(',')
 
+SECRET_KEY = str(datetime.now())
